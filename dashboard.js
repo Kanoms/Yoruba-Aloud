f9c4c57b5a515431de451ab43b1a0f7710cacf95
Kelechi <kanomneze@gmail.com>
2023-06-24 10:05:34 +0100

    dashboard functionality

--- dashboard.js
+++ dashboard.js
@@ -33,3 +33,71 @@ const learnMat = document.getElementById("learnMat");
 if (window.matchMedia("(max-width: 768px)").matches) {
   learnMat.textContent = "Learning Mat.";
 }
+
+// STEP 1
+function fetchDashboardData() {
+  // STEP 2
+  const pageModal = document.getElementById("pageModal");
+  pageModal.style.display = "flex";
+
+  // STEP 3
+  const authToken = localStorage.getItem("adminObj");
+  const tokenAcquired = JSON.parse(authToken);
+  const token = tokenAcquired.token;
+
+  // STEP 4
+  const headers = new Headers();
+
+  // STEP 5
+  headers.append("Authorization", `Bearer ${token}`);
+
+  // STEP 6
+  const request = {
+    method: "GET",
+    headers: headers,
+  };
+
+  // STEP 7
+  const URL =
+    "https://pluralcodesandbox.com/yorubalearning/api/admin/admin_dashboardapi";
+
+  // STEP 8
+  const resultData = [];
+
+  // STEP 9
+  fetch(URL, request)
+    // STEP 10
+    .then((response) => response.json())
+
+    // STEP 11
+    .then((result) => {
+      console.log(result);
+
+      // STEP 12
+      const getCategory = document.getElementById("category");
+      getCategory.innerHTML = `${result.total_number_of_categories}`;
+
+      const getLearningMaterials = document.getElementById("learningMaterials");
+      getLearningMaterials.innerHTML = `${result.total_number_of_learning_materials}`;
+
+      const getSubCategories = document.getElementById("subCategories");
+      getSubCategories.innerHTML = `${result.total_number_of_sub_categories}`;
+
+      const getTotalQuiz = document.getElementById("totalQuiz");
+      getTotalQuiz.innerHTML = `${result.total_number_of_quizzes}`;
+
+      const getTotalStudents = document.getElementById("totalStudents");
+      getTotalStudents.innerHTML = `${result.total_number_of_students}`;
+
+      const getAdminUsername = document.getElementById("adminUsername");
+      getAdminUsername.innerHTML = `${tokenAcquired.username}`;
+
+      // STEP 13
+      pageModal.style.display = "none";
+    })
+    // STEP 14
+    .catch((error) => console.log("error", error));
+}
+
+// STEP 15
+fetchDashboardData();
